59043632789f6b6d998cecbfb19bfa28eea2f6f9
Alberto Rojas <albertorojasm95@gmail.com>
2015-02-11 17:42:36 -0600

    Programa Listo!

--- CuadroMagico.py
+++ CuadroMagico.py
@@ -1,8 +1,4 @@
 # -*- coding: cp1252 -*-
-#Crear cuadro magico de 1 a 9
-#Empieza en el centro con 1 y luego uno arriba y a la izq, si no abajo
-#2,3,4,5...
-#El primer [fila] [columna] [[fila]columna]
 
 print "CUADRO MAGICO"
 tamano=int(raw_input("Ingrese el tamaño del Cuadro: "))
@@ -16,58 +12,27 @@ columna=centro
 ubacion=0
 
 for i in range((tamano*tamano)-1):    
-    for n in range(tamano):    
-        print str(cuadro[0:tamano][n])
-    print "------------------------------"
     num=num+1
-    print "VA EL NUMERO"+str(num)
-    print "principio"
     ubifila=fila
-    print "ubicacion"
-    print ubifila
     fila=fila-1
-    print "fila1"
-    print fila
     ubicolumna=columna
-    print "ubicacion"
-    print ubicolumna
     columna=columna-1
-    print "columna1"
-    print columna
     if fila<0:        
         fila=tamano-1
-        print "fila2"
-        print fila
     if columna<0:        
         columna=tamano-1
-        print "columna2"
-        print columna
     else:        
-        columna=ubicolumna-1
-        print "columna3"
-        print columna
-    print "desnalgue"        
+        columna=ubicolumna-1       
     if cuadro[fila][columna]==0:
-        print "PRIMER IF ==0"
-        cuadro[fila][columna]=num
-        
+        cuadro[fila][columna]=num       
     elif cuadro[fila][columna]!=0:
-        print "SEGUNDO IF !=0"
         fila=fila+1
-        print "fila3"
-        print fila
         if fila>0:        
             fila=ubifila+1
-        print "fila4"
-        print fila
         columna=ubicolumna
-        print cuadro[-1][columna]
         cuadro[fila][columna]=num
        
-
-
 for n in range(tamano):    
     print str(cuadro[0:tamano][n])
-
    
 raw_input("Press Enter to continue...")
