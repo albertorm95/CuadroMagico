9b05e103abc965a67432027a2c598ab085c59ac7
unknown <CISCO@CISCO-03.uagtabasco.edu.mx>
2015-02-12 10:10:18 -0600

    Eliminacion de lineas innecesarias, y agregado de comentarios auxiliares. Agregado de README (no terminado)
    Signed-off-by: Alberto Rojas <CISCO@CISCO-03.uagtabasco.edu.mx>

--- CuadroMagico.py
+++ CuadroMagico.py
@@ -1,28 +1,30 @@
 # -*- coding: cp1252 -*-
 
+#Inicio:
 print "CUADRO MAGICO"
 tamano=int(raw_input("Ingrese el tamaño del Cuadro: "))
 centro=int(tamano/2)
 cuadro=[[0 for i in range(tamano)] for i in range(tamano)]
 cuadro[0][centro]=1
 
+#Variables:
 num=1
 fila=0
 columna=centro
-ubacion=0
 
+#Proceso:
 for i in range((tamano*tamano)-1):    
     num=num+1
     ubifila=fila
     fila=fila-1
     ubicolumna=columna
     columna=columna-1
+    #Logica de ubicacion
     if fila<0:        
         fila=tamano-1
     if columna<0:        
         columna=tamano-1
-    else:        
-        columna=ubicolumna-1       
+    #Logica de llenado
     if cuadro[fila][columna]==0:
         cuadro[fila][columna]=num       
     elif cuadro[fila][columna]!=0:
@@ -31,7 +33,8 @@ for i in range((tamano*tamano)-1):
             fila=ubifila+1
         columna=ubicolumna
         cuadro[fila][columna]=num
-       
+
+#Impresion:
 for n in range(tamano):    
     print str(cuadro[0:tamano][n])
    
---
+++ README.txt
@@ -0,0 +1,41 @@
+Cuadro Mágico
+
+-----------------------------------------
+
+Descripción:
+
+Un algoritmo que se encarga de crear un cuadro magico de multiples tamaños, un cuadro magico es un cuadro horizontal, vertical y diagonal seran la misma.
+
+-----------------------------------------
+
+Como funciona:
+
+Se inicia solicitando el tamaño del Cuadro Magico
+
+-----------------------------------------
+
+Entorno:
+
+Python.
+Git.
+
+-----------------------------------------
+
+Desarrollado por:
+
+Alberto Manuel Rojas Mendez
+
+-----------------------------------------
+
+Proposito:
+
+Estructura de Datos
+Experiencia de Apendrizaje
+Febrero - Febrero 2015
+Universidad Autonoma de Guadalajara, Campus Tabasco
+
+-----------------------------------------
+
+Ubicación: https://github.com/albertorm95/CuadroMagico
+
+-----------------------------------------
